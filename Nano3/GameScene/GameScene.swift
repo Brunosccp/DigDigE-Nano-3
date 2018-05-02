9c515184855472fff5fc3297abf2b5ae5573e486
Brunosccp <rocca_bruno@hotmail.com>
2018-05-02 11:07:23 -0300

    correções sobre o score e mult

--- Nano3/GameScene/GameScene.swift
+++ Nano3/GameScene/GameScene.swift
@@ -190,6 +190,7 @@ extension GameScene : SKPhysicsContactDelegate{
         }
         if(bodyA == wrongCategory){ //quando a bola bate no segundo retangulo invisivel
             currentNote.removeFirst()
+            updateMultiplier(rightNote: false)
         }
     }
 }
